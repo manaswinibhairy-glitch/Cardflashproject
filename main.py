9f429a53d590021cfc7da9a8531b0339c1e0cb26
Bhairy Srimanaswini <manaswinibhairy@gmail.com>
2026-09-12 14:10:20 +0530

    .idea/workspace.xml

---
+++ main.py
@@ -0,0 +1,63 @@
+from tkinter import Tk, Canvas, PhotoImage, Button
+import pandas as pd
+import random
+
+BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+to_learn = {}
+
+# Load data
+try:
+    data = pd.read_csv("words_to_learn.csv")
+except FileNotFoundError:
+    data = pd.read_csv("french_words.csv")
+to_learn = data.to_dict(orient="records")
+
+# Next card function
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, flip_card)
+
+# Flip card function
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
+
+# Known word function
+def is_known():
+    to_learn.remove(current_card)
+    pd.DataFrame(to_learn).to_csv("words_to_learn.csv", index=False)
+    next_card()
+
+# UI setup
+window = Tk()
+window.title("Flashcards")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+flip_timer = window.after(3000, flip_card)
+
+canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+card_front_img = PhotoImage(file="card_front.png")
+card_back_img = PhotoImage(file="card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
+card_title = canvas.create_text(400, 150, text="", font=("Arial", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Arial", 60, "bold"))
+canvas.grid(row=0, column=0, columnspan=2)
+
+# Buttons
+wrong_img = PhotoImage(file="wrong.png")
+right_img = PhotoImage(file="right.png")
+wrong_button = Button(image=wrong_img, highlightthickness=0, command=next_card)
+wrong_button.grid(row=1, column=0)
+right_button = Button(image=right_img, highlightthickness=0, command=is_known)
+right_button.grid(row=1, column=1)
+
+next_card()
+
+window.mainloop()
